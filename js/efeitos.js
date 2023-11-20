3913a7afacb4127328a8cfbea5df16c66bc70fb1
Jonas Carvalho <jonasgabrielsc@gmail.com>
2023-11-20 00:40:04 -0300

    organização da api google maps

--- js/efeitos.js
+++ js/efeitos.js
@@ -33,27 +33,7 @@ function alternarTexto(event) {
   }
   window.scrollTo(0, inicio);
   event.preventDefault();
-}
-
-// Localização no google maps
-function initMap() {
-    var myLatLng = {lat: -12.933844305394144, lng: -38.50172459197853};
-  
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: myLatLng,
-      zoom: 15,
-    });
-  
-    var marker = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      title: 'Lesampê'
-    });
-  }
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    initMap();
-  });  
+}  
   
 /* Abrir e fechar ícones serviços home */
 var coll = document.getElementsByClassName('icones-servicos');
